36a2aa31475e85bbf6b4b4bb2094300982aa2746
Asaf Hadad <asaf147369@gmail.com>
2023-01-13 17:35:15 +0200

    changed

--- lib/presentation/widgets/drawer_content.dart
+++ lib/presentation/widgets/drawer_content.dart
@@ -9,26 +9,68 @@ class DrawerContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        children: <Widget>[
-          const DrawerHeader(
-            child: Text('Bloons TD 6 Wiki By Asaf Hadad'),
-          ),
-          ...pages.map(
-            (page) => ListTile(
-                title: Text(titles[pages.indexOf(page)]),
+        child: ListView(children: <Widget>[
+      const DrawerHeader(
+        child: Text('Bloons TD 6 Wiki By Asaf Hadad'),
+      ),
+      // ...pages.map(
+      //   (page) => ListTile(
+      //       title: Text(titles[pages.indexOf(page)]),
+      //       onTap: () {
+      //         Navigator.pop(context);
+      //         GlobalState.currentPageIndex = pages.indexOf(page);
+      //         pageController.animateToPage(
+      //           GlobalState.currentPageIndex,
+      //           duration: const Duration(milliseconds: 500),
+      //           curve: Curves.easeInOut,
+      //         );
+      //       }),
+      // ),
+      ExpansionTile(
+        title: Text(titles[0]),
+        children: [
+          ListView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: GlobalState.towerTypes.length,
+            shrinkWrap: true,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(GlobalState.towerTypes[index]),
                 onTap: () {
                   Navigator.pop(context);
-                  GlobalState.currentPageIndex = pages.indexOf(page);
-                  pageController.animateToPage(
-                    GlobalState.currentPageIndex,
-                    duration: const Duration(milliseconds: 500),
-                    curve: Curves.easeInOut,
-                  );
-                }),
+                  GlobalState.currentPageIndex = 0;
+                  GlobalState.currentTitle = titles[0];
+                  pageController.animateToPage(0,
+                      duration: const Duration(milliseconds: 300),
+                      curve: Curves.easeInOut);
+                },
+              );
+            },
           ),
         ],
       ),
-    );
+      ListTile(
+          title: Text(titles[1]),
+          onTap: () {
+            Navigator.pop(context);
+            GlobalState.currentPageIndex = 1;
+            pageController.animateToPage(
+              GlobalState.currentPageIndex,
+              duration: const Duration(milliseconds: 500),
+              curve: Curves.easeInOut,
+            );
+          }),
+      ListTile(
+          title: Text(titles[2]),
+          onTap: () {
+            Navigator.pop(context);
+            GlobalState.currentPageIndex = 2;
+            pageController.animateToPage(
+              GlobalState.currentPageIndex,
+              duration: const Duration(milliseconds: 500),
+              curve: Curves.easeInOut,
+            );
+          }),
+    ]));
   }
 }
