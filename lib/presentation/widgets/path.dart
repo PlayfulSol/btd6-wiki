22daeba66aede243f4d9169cb7dd70630ab6620d
Asaf Hadad <asaf147369@gmail.com>
2023-01-15 02:09:46 +0200

    add tiles to paths

--- lib/presentation/widgets/path.dart
+++ lib/presentation/widgets/path.dart
@@ -18,59 +18,59 @@ class MonkeyPath extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      const SizedBox(height: 10),
-      Text(getPathTitleFromKey(pathKey),
-          style: const TextStyle(
-              fontSize: 24, fontWeight: FontWeight.bold, color: Colors.teal)),
-      const SizedBox(height: 10),
-      ListView.builder(
-        shrinkWrap: true,
-        primary: false,
-        itemCount: path.length,
-        itemBuilder: (context, index) => Column(
-          children: [
-            Text(path[index].name, style: const TextStyle(fontSize: 18)),
-            const SizedBox(height: 10),
-            Image.network(towerPathImage(monkeyId, pathKey, index), width: 200),
-            const SizedBox(height: 10),
-            Text(
-              path[index].description,
-              textAlign: TextAlign.center,
-              style: const TextStyle(fontSize: 15),
-            ),
-            const SizedBox(height: 10),
-            const Text("Cost:"),
-            Text(
-              costToString(path[index].cost),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 30),
-            // a closed list of all the effects
-            if (path[index].effects.isNotEmpty) ...[
-              ExpansionTile(
-                title: const Text("Effects:"),
-                children: [
-                  ListView.builder(
-                      shrinkWrap: true,
-                      primary: false,
-                      itemCount: path[index].effects.length,
-                      itemBuilder: (context, effectIndex) => Column(
-                            children: [
-                              Text(
-                                path[index].effects[effectIndex],
-                                textAlign: TextAlign.center,
-                              ),
-                              const SizedBox(height: 15),
-                            ],
-                          )),
+    return ExpansionTile(
+        title: Text(getPathTitleFromKey(pathKey),
+            style: const TextStyle(
+                fontSize: 24, fontWeight: FontWeight.bold, color: Colors.teal)),
+        children: [
+          ListView.builder(
+            shrinkWrap: true,
+            primary: false,
+            itemCount: path.length,
+            itemBuilder: (context, index) => Column(
+              children: [
+                Text(path[index].name, style: const TextStyle(fontSize: 18)),
+                const SizedBox(height: 10),
+                Image.network(towerPathImage(monkeyId, pathKey, index),
+                    width: 200),
+                const SizedBox(height: 10),
+                Text(
+                  path[index].description,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontSize: 15),
+                ),
+                const SizedBox(height: 10),
+                const Text("Cost:"),
+                Text(
+                  costToString(path[index].cost),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 30),
+                // a closed list of all the effects
+                if (path[index].effects.isNotEmpty) ...[
+                  ExpansionTile(
+                    title: const Text("Effects:"),
+                    children: [
+                      ListView.builder(
+                          shrinkWrap: true,
+                          primary: false,
+                          itemCount: path[index].effects.length,
+                          itemBuilder: (context, effectIndex) => Column(
+                                children: [
+                                  Text(
+                                    path[index].effects[effectIndex],
+                                    textAlign: TextAlign.center,
+                                  ),
+                                  const SizedBox(height: 15),
+                                ],
+                              )),
+                    ],
+                  ),
+                  const SizedBox(height: 30),
                 ],
-              ),
-              const SizedBox(height: 30),
-            ],
-          ],
-        ),
-      ),
-    ]);
+              ],
+            ),
+          ),
+        ]);
   }
 }
