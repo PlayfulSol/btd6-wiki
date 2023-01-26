64c88f36bad4fdc677d4e66f4973a502e124f23b
Asaf Hadad <asaf147369@gmail.com>
2023-01-26 14:32:43 +0200

    change to bloons page layout

--- lib/presentation/screens/bloon/bloons.dart
+++ lib/presentation/screens/bloon/bloons.dart
@@ -19,115 +19,115 @@ class _BloonsState extends State<Bloons> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(children: [
-          const Text("Bloons", style: TextStyle(fontSize: 20)),
-          const SizedBox(height: 15),
-          FutureBuilder(
-            future: Future.value(GlobalState.bloons),
-            builder: (BuildContext context, AsyncSnapshot snapshot) {
-              if (snapshot.data == null) {
-                return const Loader();
-              } else {
-                return GridView.builder(
-                    itemCount: snapshot.data.length,
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      childAspectRatio: 1,
-                      mainAxisExtent: 80,
-                    ),
-                    shrinkWrap: true,
-                    primary: false,
-                    itemBuilder: (context, index) {
-                      return Center(
-                        child: Card(
-                          child: Column(
-                            mainAxisSize: MainAxisSize.max,
-                            children: <Widget>[
-                              ListTile(
-                                mouseCursor: SystemMouseCursors.click,
-                                leading: CircleAvatar(
-                                  backgroundColor: Colors.transparent,
-                                  backgroundImage: NetworkImage(
-                                      bloonImage(snapshot.data[index].id)),
-                                ),
-                                title: Text(snapshot.data[index].name,
-                                    style: const TextStyle(fontSize: 14)),
-                                subtitle: Text(snapshot.data[index].type),
-                                onTap: () {
-                                  if (!GlobalState.isLoading) {
-                                    getBloonData(snapshot.data[index].id).then(
-                                        (value) => Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                                builder: (context) =>
-                                                    SingleBloon(
-                                                      bloon: value,
-                                                    ))));
-                                  }
-                                },
+        body: Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: ListView(children: [
+        const Text(
+          "Bloons",
+          style: TextStyle(fontSize: 20),
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(height: 15),
+        FutureBuilder(
+          future: Future.value(GlobalState.bloons),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.data == null) {
+              return const Loader();
+            } else {
+              return GridView.builder(
+                  itemCount: snapshot.data.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 1,
+                    mainAxisExtent: 80,
+                  ),
+                  shrinkWrap: true,
+                  primary: false,
+                  itemBuilder: (context, index) {
+                    return Center(
+                      child: Card(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          children: <Widget>[
+                            ListTile(
+                              mouseCursor: SystemMouseCursors.click,
+                              leading: CircleAvatar(
+                                backgroundColor: Colors.transparent,
+                                backgroundImage: NetworkImage(
+                                    bloonImage(snapshot.data[index].id)),
                               ),
-                            ],
-                          ),
+                              title: Text(snapshot.data[index].name,
+                                  style: const TextStyle(fontSize: 14)),
+                              subtitle: Text(snapshot.data[index].type),
+                              onTap: () {
+                                if (!GlobalState.isLoading) {
+                                  getBloonData(snapshot.data[index].id)
+                                      .then((value) => Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => SingleBloon(
+                                                    bloon: value,
+                                                  ))));
+                                }
+                              },
+                            ),
+                          ],
                         ),
-                      );
-                    });
-              }
-            },
-          ),
-          const SizedBox(height: 30),
-          const Text("Bosses", style: TextStyle(fontSize: 20)),
-          const SizedBox(height: 15),
-          FutureBuilder(
-            future: Future.value(GlobalState.bosses),
-            builder: (BuildContext context, AsyncSnapshot snapshot) {
-              if (snapshot.data == null) {
-                return const Loader();
-              } else {
-                return GridView.builder(
-                    itemCount: snapshot.data.length,
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 1,
-                      childAspectRatio: 1,
-                      mainAxisExtent: 80,
-                    ),
-                    shrinkWrap: true,
-                    primary: false,
-                    itemBuilder: (context, index) {
-                      return Card(
-                        child: ListTile(
-                          mouseCursor: SystemMouseCursors.click,
-                          leading: CircleAvatar(
-                            backgroundColor: Colors.transparent,
-                            backgroundImage: NetworkImage(
-                                bloonImage(snapshot.data[index].id)),
-                          ),
-                          title: Text(snapshot.data[index].name,
-                              style: const TextStyle(fontSize: 14)),
-                          subtitle: Text(snapshot.data[index].type),
-                          onTap: () {
-                            if (!GlobalState.isLoading) {
-                              getBloonData(snapshot.data[index].id)
-                                  .then((value) => Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => BossBloon(
-                                                bloon: value,
-                                              ))));
-                            }
-                          },
+                      ),
+                    );
+                  });
+            }
+          },
+        ),
+        const SizedBox(height: 30),
+        const Text("Bosses",
+            style: TextStyle(fontSize: 20), textAlign: TextAlign.center),
+        const SizedBox(height: 15),
+        FutureBuilder(
+          future: Future.value(GlobalState.bosses),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.data == null) {
+              return const Loader();
+            } else {
+              return GridView.builder(
+                  itemCount: snapshot.data.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 1,
+                    childAspectRatio: 1,
+                    mainAxisExtent: 80,
+                  ),
+                  shrinkWrap: true,
+                  primary: false,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: ListTile(
+                        mouseCursor: SystemMouseCursors.click,
+                        leading: CircleAvatar(
+                          backgroundColor: Colors.transparent,
+                          backgroundImage:
+                              NetworkImage(bloonImage(snapshot.data[index].id)),
                         ),
-                      );
-                    });
-              }
-            },
-          ),
-        ]),
-      ),
+                        title: Text(snapshot.data[index].name,
+                            style: const TextStyle(fontSize: 14)),
+                        subtitle: Text(snapshot.data[index].type),
+                        onTap: () {
+                          if (!GlobalState.isLoading) {
+                            getBloonData(snapshot.data[index].id)
+                                .then((value) => Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => BossBloon(
+                                              bloon: value,
+                                            ))));
+                          }
+                        },
+                      ),
+                    );
+                  });
+            }
+          },
+        ),
+      ]),
     ));
   }
 }
